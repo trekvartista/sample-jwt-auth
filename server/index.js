7c49ac5940020041c7d268d94e411d7b894bc64e
trekvartista <isken_150502@mail.ru>
2022-06-26 23:59:04 +0600

    Initial project setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const sequelize = require('./db')
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+
+const start = async () => {
+	try {
+		await sequelize.authenticate()
+		await sequelize.sync()
+
+		app.listen(PORT, () => console.log('Server started on port: ' + PORT))
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start()

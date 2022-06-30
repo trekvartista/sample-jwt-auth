dc98ccd80181ae526a60bc0b12c20f1c67104c9d
trekvartista <isken_150502@mail.ru>
2022-06-30 23:49:18 +0600

    auth api is done

---
+++ client/src/api/index.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const API_URL = "http://localhost:5000";
+
+const api = axios.create({
+    withCredentials: true, // to attach cookies to every request
+    baseURL: "http://localhost:5000",
+});
+
+api.interceptors.response.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+});
+
+export default api;
